1bd39f4d4a073439aa4abd63ea7e03e67ea15902
frin-network <153652184+frin-network@users.noreply.github.com>
2023-12-12 23:11:55 +0600

    Delete domain/consensus/processes/model directory

--- domain/consensus/processes/model/interface_processes_parentsmanager.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "github.com/frin-network/frind/domain/consensus/model/externalapi"
-
-// ParentsManager lets is a wrapper above header parents that replaces empty parents with genesis when needed.
-type ParentsManager interface {
-	ParentsAtLevel(blockHeader externalapi.BlockHeader, level int) externalapi.BlockLevelParents
-	Parents(blockHeader externalapi.BlockHeader) []externalapi.BlockLevelParents
-}
